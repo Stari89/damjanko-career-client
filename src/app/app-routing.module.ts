365503e4b0b20d9de408f535c2ee1dfb64e0ead5
Damjan Kovacic <letecnez@gmail.com>
2018-04-02 11:05:50 +0200

    Basic routing and test navigation

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IndexComponent } from './index/index.component';
+import { CurriculumVitaeComponent } from './curriculum-vitae/curriculum-vitae.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+	{ path: '', component: IndexComponent, pathMatch: 'full' },
+	{ path: 'curriculum-vitae', component: CurriculumVitaeComponent },
+	{ path: 'about', component: AboutComponent },
+	{ path: 'login', component: LoginComponent },
+	{ path: "**", component: PageNotFoundComponent },
+];
+
+@NgModule({
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
+  export const routingComponents = [ IndexComponent, CurriculumVitaeComponent, AboutComponent, LoginComponent, PageNotFoundComponent ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 
 
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CurriculumVitaeComponent } from './curriculum-vitae/curriculum-vitae.component';
+import { IndexComponent } from './index/index.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    CurriculumVitaeComponent,
+    IndexComponent,
+    AboutComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
